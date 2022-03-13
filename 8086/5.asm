8dd8665a152f3eb6b9717a0d5ead9fd0c037f725
Suraj-Pahari <surajpahari152@gmail.com>
2022-03-13 12:29:43 +0545

    Fixing Q5 solution

--- 8086/5.asm
+++ 8086/5.asm
@@ -7,18 +7,29 @@ print macro msg
        int 21h
        inc di
        loop for
+
+  add di, 16h
+  mov cx, 0Ch
+
+  again: mov dl, [di]
+	 mov ah, 02h
+	 int 21h
+	 inc di
+	 loop again
 endm
 
 .model small
 .stack 64h
 .data
-  str db 'Microprocessor and Assemble Language Programming$'
+  str db 'Microprocessor and Assembly Language Programming$'
 .code
   main proc
     mov ax, @data
     mov ds, ax
     
-    disp str
-
+    print str
+    
+    mov ax, 4c00h
+    int 21h
   main endp
 end main
