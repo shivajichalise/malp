0ba2dca63b9a87122f899d2baaf6ea9847f9e701
Shivaji Chalise <respond2shivaji@gmail.com>
2022-04-10 06:33:59 +0545

    fixes

--- 8086/7.asm
+++ 8086/7.asm
@@ -1,46 +1,43 @@
-.model small
-.stack 64h
+.model
+.stack 100h
 .data
-  msg1 db 10,13, "Enter the number: $"
-  msg2 db 10,13, "Square of given number is: $"
+  one db 1
+  res dw ?
 .code
   main proc
     mov ax, @data
     mov ds, ax
 
-    lea dx, msg1
-    mov ah, 09h
-    int 21h
-
     mov ah, 01h
     int 21h
-    
     sub al, 30h
-    mov bl, al
-    call findsquare
-    mov bx, ax
+    sub ah, ah
+    
+    mov cx, 0
+    mov cl, al
+    sub ch, ch
 
-    lea dx, msg2
-    mov ah, 09h
-    int 21h
+    mov bx, 0000h
+    mov ax, 0000h
+
+    for: mov al, one
+      mul al
+      add bx, ax
+      inc one
+      loop for
+
+    add bx, 3030h
+    mov res, bx
     
     mov dl, bh
-    add dl, 30h
     mov ah, 02h
     int 21h
 
     mov dl, bl
-    add dl, 30h
     mov ah, 02h
     int 21h
-
+    
     mov ax, 4c00h
     int 21h
   main endp
-
-  findsquare proc
-   mul bl
-   aam
-  ret
-  findsquare endp
 end main
